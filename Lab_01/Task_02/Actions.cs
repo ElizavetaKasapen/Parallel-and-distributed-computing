1018710bfc6a84d267b65cd5ff7e4633b262471a
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-03-05 21:15:42 +0200

    Add files via upload

---
+++ Lab_01/Task_02/Actions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_01_02
+{
+    class Actions
+    {
+        public void Sum_using_array(int size)
+        {
+            double[] array = new double[size];
+            for (int i = 0; i < size; i++)
+                array[i] = i;
+            double sum = 0;
+            for (int i = 0; i < size; i++)
+                sum += array[i];
+        }
+
+        public void Sum_using_linked_list(int size)
+        {
+            LinkedList<Double> linked_list = new LinkedList<Double>();
+            for (int i = 0; i < size; i++)
+                linked_list.AddLast(Convert.ToDouble(i));
+            double sum = 0;
+            foreach (double i in linked_list)
+                sum += i;
+        }
+
+        public void Sum_using_array_list(int size)
+        {
+            ArrayList list = new ArrayList(size);
+            for (int i = 0; i < size; i++)
+                list.Add(Convert.ToDouble(i));
+            double sum = 0;
+            foreach (double i in list)
+                sum += i;
+        }
+    }
+}
---
+++ Lab_01/Task_02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Lab_01;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Lab_01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actions actions = new Actions();
+            Stopwatch stopwatch = new Stopwatch();
+            Work_with_file work_with_file = new Work_with_file();
+            Dictionary<int, double> using_array = new Dictionary<int, double>();
+            Dictionary<int, double> using_linked_list = new Dictionary<int, double>();
+            Dictionary<int, double> using_array_list = new Dictionary<int, double>();
+
+            for (int i = 0; i <= 12000; i += 50)
+            {
+                int bytes = i * i * 8 / 1024; // 8 because double
+                //using array
+                stopwatch.Restart();
+                actions.Sum_using_array(i);
+                stopwatch.Stop();
+                using_array.Add(bytes, stopwatch.Elapsed.TotalMilliseconds);
+
+                //using_linked_list
+                stopwatch.Restart();
+                actions.Sum_using_linked_list(i);
+                stopwatch.Stop();
+                using_linked_list.Add(bytes, stopwatch.Elapsed.TotalMilliseconds);
+
+                //using_array_list
+                stopwatch.Restart();
+                actions.Sum_using_array_list(i);
+                stopwatch.Stop();
+                using_array_list.Add(bytes, stopwatch.Elapsed.TotalMilliseconds);
+            }
+            //download data to files
+            work_with_file.Write_to_file(using_array, "using_array.txt");
+            work_with_file.Write_to_file(using_linked_list, "using_linked_list.txt");
+            work_with_file.Write_to_file(using_array_list, "using_array_list.txt");
+        }
+    }
+}
