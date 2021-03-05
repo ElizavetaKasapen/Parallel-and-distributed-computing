66c1e3653f5f2ed02d58a6abec2aee2839cab1dc
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-03-05 21:16:51 +0200

    Add files via upload

---
+++ Lab_01/Task_03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab_01_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestHashSet testHashSet = new TestHashSet();
+            Console.WriteLine("Test check all: "+ testHashSet.Test_check_all().ToString()
+                                + "\nTest check single: "+ testHashSet.Test_check_single().ToString());
+        }
+    }
+}
---
+++ Lab_01/Task_03/TestHashSet.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace Lab_01_03
+{
+    class TestHashSet
+    {
+        public double Test_check_all(){
+            Stopwatch stopwatch = new Stopwatch();
+            HashSet<long> hashstructure = Create();
+            //timer
+            stopwatch.Restart();
+            for (int i = 0; i < 10000000; i++)
+            {
+                long elem = i;
+                Assert.IsTrue(hashstructure.Contains(elem));
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        public double Test_check_single()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            HashSet<long> hashstructure = Create();
+            //timer
+            stopwatch.Restart();
+            long elem = 0;
+            for (int i = 0; i < 10000000; i++)
+            {
+                Assert.IsTrue(hashstructure.Contains(elem));
+            }
+            stopwatch.Stop();
+            return stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        private HashSet<long> Create()
+        {
+            HashSet<long> hashstructure = new HashSet<long>();
+            // Inserting elements in HashSet 
+            for (int i = 0; i < 10000000; i++)
+            {
+                hashstructure.Add(i);
+            }
+            return hashstructure;
+        }
+    }
+}
