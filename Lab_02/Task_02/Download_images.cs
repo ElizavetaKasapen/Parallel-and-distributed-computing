69770dfb6677579b25b653947711907506ff06e5
ElizavetaKasapen <40471098+ElizavetaKasapen@users.noreply.github.com>
2021-03-15 21:37:21 +0200

    Add files via upload

---
+++ Lab_02/Task_02/Download_images.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Lab_02_02
+{
+    class Download_images
+    {
+        
+        
+        public void Remote(String url)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile(url, "D:\\University\\4.2\\Pictures\\" + Path.GetFileName(url));
+            }
+        }
+
+        public void Local(String path)
+        {
+                Bitmap img = new Bitmap(path);
+                img.Save(@"D:\University\4.2\Pictures_to_save\" + Path.GetFileName(path));
+        }
+    }
+}
